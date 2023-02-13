5fe87e4373a3cc6ac5247ca57b9168bf2ebfb38e
Alex Zemak <123azemak@frhsd.com>
2023-02-13 10:59:07 -0500

    followed along with some of the lesson

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = Vue.createApp({
+data() {
+    return {
+        title: "weh",
+        author: "brandon lulu",
+        age: 2
+    }
+},
+methods: {
+    //something
+}
+});
+
+app.mount('#app');
