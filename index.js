1e9c2bc7d616e6f7c111e37a4fb46e0fa95a4319
Alex Zemak <123azemak@frhsd.com>
2023-02-15 08:26:42 -0500

    Finished vue directives lesson, eeby deeby

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var app = Vue.createApp({
 data() {
     return {
+        showBooks: true,
         title: "weh",
         author: "brandon lulu",
         age: 2
     }
 },
 methods: {
-    //something
+    toggleShowBooks(){
+        this.showBooks = !this.showBooks;
+    }
 }
+// methods: {
+//     changeTitle(title){
+//         // this.title = 'The Final Shape';
+//         this.title = title
+//     }
+// }
 });
 
 app.mount('#app');
