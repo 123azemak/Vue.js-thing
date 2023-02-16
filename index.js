42e0d643f289ec560a6c7e7b91a30eaf9eb32030
Alex Zemak <123azemak@frhsd.com>
2023-02-16 09:47:26 -0500

    almost finised video 3, still binding imgs

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 var app = Vue.createApp({
 data() {
     return {
+        url: 'http://www.thenetninja.co.uk',
         showBooks: true,
-        title: "weh",
-        author: "brandon lulu",
-        age: 2
+        // title: "weh",
+        // author: "brandon lulu",
+        // age: 2,
+        // x: 0,
+        // y: 0
+        books : [
+            {title: 'eeby deeby', author: 'brandon lu' img: ''},
+            {title: 'eeby deeby', author: 'brandon lu' img: ''},
+            {title: 'eeby deeby', author: 'brandon lu' img: ''}
+        ]
     }
 },
 methods: {
     toggleShowBooks(){
         this.showBooks = !this.showBooks;
-    }
+    },
+//     handleEvent(e, data){
+//         console.log(e, e.type);
+//         if (data) {
+//             console.log(data);
+//         }
+//     },
+//     handleMousemove(e){
+// this.x = e.offsetX;
+// this.y = e.offsetY;
+//     }
 }
 // methods: {
 //     changeTitle(title){
